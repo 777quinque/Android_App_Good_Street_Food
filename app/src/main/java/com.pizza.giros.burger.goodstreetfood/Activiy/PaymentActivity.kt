088371ed62687183eb4d1e495a384a313a55e199
777quinque <xchaos228@gmail.com>
2025-01-22 22:02:40 +0500

    Update PaymentActivity.kt
    use your terminal and pass for tinkoff pay

--- app/src/main/java/com.pizza.giros.burger.goodstreetfood/Activiy/PaymentActivity.kt
+++ app/src/main/java/com.pizza.giros.burger.goodstreetfood/Activiy/PaymentActivity.kt
@@ -75,7 +75,7 @@ class PaymentActivity : AppCompatActivity() {
         val ORDERID = System.currentTimeMillis().toString()
         val totalTxt = intent.getIntExtra("TOTAL_AMOUNT", 0)
         val options = PaymentOptions().setOptions {
-            setTerminalParams("1714766466399", "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAv5yse9ka3ZQE0feuGtemYv3IqOlLck8zHUM7lTr0za6lXTszRSXfUO7jMb+L5C7e2QNFs+7sIX2OQJ6a+HG8kr+jwJ4tS3cVsWtd9NXpsU40PE4MeNr5RqiNXjcDxA+L4OsEm/BlyFOEOh2epGyYUd5/iO3OiQFRNicomT2saQYAeqIwuELPs1XpLk9HLx5qPbm8fRrQhjeUD5TLO8b+4yCnObe8vy/BMUwBfq+ieWADIjwWCMp2KTpMGLz48qnaD9kdrYJ0iyHqzb2mkDhdIzkim24A3lWoYitJCBrrB2xM05sm9+OdCI1f7nPNJbl5URHobSwR94IRGT7CJcUjvwIDAQAB")
+            setTerminalParams("your terminal", "your pass")
             orderOptions {
                 orderId = ORDERID
                 title = "Ваш заказ"
